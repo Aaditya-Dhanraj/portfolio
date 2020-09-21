b44a1d778c4846668fd410e06b5a6f6cdcc6d0f9
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-09-21 16:10:14 +0530

    Updates

---
+++ precache-manifest.afd5d7766794d0fc824b77eda11c7615.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9075df874049c60dc4133b46c29e740d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "11501a51595c87b710de",
+    "url": "/portfolio/static/css/main.fea4070a.chunk.css"
+  },
+  {
+    "revision": "79c760f4e4d31513713f",
+    "url": "/portfolio/static/js/2.4afb6d09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4afb6d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11501a51595c87b710de",
+    "url": "/portfolio/static/js/main.6c624775.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "014560c03cbcab7f1adee1b2824cc5d6",
+    "url": "/portfolio/static/media/brain.014560c0.svg"
+  },
+  {
+    "revision": "b8ba61fe1f9d0e5c8416ff2713170dad",
+    "url": "/portfolio/static/media/github.b8ba61fe.svg"
+  },
+  {
+    "revision": "61889089f7a6aaabcf6ba1da68204187",
+    "url": "/portfolio/static/media/github2.61889089.png"
+  },
+  {
+    "revision": "aa253b967939712842fbcf4188888bc6",
+    "url": "/portfolio/static/media/instagram.aa253b96.svg"
+  },
+  {
+    "revision": "810274f08ccc5aef3c797e431fe342b2",
+    "url": "/portfolio/static/media/link.810274f0.svg"
+  },
+  {
+    "revision": "d72aeebd22e4a818717090eda2a226de",
+    "url": "/portfolio/static/media/linkedin.d72aeebd.svg"
+  },
+  {
+    "revision": "0845d8e23c92427a5c85d66b628c373d",
+    "url": "/portfolio/static/media/pic2.0845d8e2.svg"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/portfolio/static/media/tripper.40d4ef82.png"
+  }
+]);
