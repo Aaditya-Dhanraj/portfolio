71a60159b9a2e9fa8dbe93eb8ae570884bfb3f1e
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-09-22 15:09:20 +0530

    glitches fixed

--- src/sections/section1/section1.js
+++ src/sections/section1/section1.js
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
 import Parallax from "react-rellax";
 import Floater from "../../animations/floating";
-import pic1 from "../../assets/pic2.svg";
+import pic1 from "../../assets/Aaditya.svg";
 import github from "../../assets/github.svg";
 import linkdin from "../../assets/linkedin.svg";
 import instagram from "../../assets/instagram.svg";
 import "./section1.css";
-import TypeWriterEffect from "react-typewriter-effect";
+import { init } from "ityped";
 
 class Sec1 extends Component {
+  componentDidMount() {
+    const myElement = document.querySelector("#myElement");
+    init(myElement, {
+      showCursor: true,
+      strings: ["Frontend", "Backend", "Fullstack"],
+    });
+  }
   render() {
     return (
       <div className="parent1">
         <div className="section1">
           <div className="left-side ">
             <Parallax speed={7}>
-              <div className="flex" style={{ paddingTop: "8vh" }}>
+              <div className="flex hello">
                 <span
                   className="mono"
                   style={{ paddingRight: "8px", paddingTop: "5px" }}
@@ -38,81 +45,16 @@ class Sec1 extends Component {
                 >
                   I am a
                 </span>
-                <TypeWriterEffect
+                <div id="myElement"
                   className="left"
-                  textStyle={{
+                  style={{
                     fontFamily: "Times New Roman, Times, serif",
                     color: "black",
                     fontWeight: "bolder",
                     fontSize: "1.5em",
-                  }}
-                  startDelay={0}
-                  cursorColor="black"
-                  multiText={[
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                    "Frontend",
-                    "Backend",
-                    "Fullstack",
-                  ]}
-                  multiTextDelay={2000}
-                  typeSpeed={50}
-                />
+                  }} >
+                 
+                  </div>
                 <span
                   style={{
                     fontFamily: "Times New Roman, Times, serif",
@@ -126,11 +68,20 @@ class Sec1 extends Component {
               </div>
             </Parallax>
             <Parallax>
-           <div className="social" >
-           <a href="https://github.com/Aaditya-Dhanraj" target="_blank" ><img alt=""  className="socialicon" src={github} /></a>
-          <a href="https://www.google.com" target="_blank" ><img alt="" className="socialicon" src={linkdin} /></a>
-           <a href="https://www.instagram.com/its_aditude/" target="_blank" ><img alt="" className="socialicon" src={instagram} /></a>
-           </div>
+              <div className="social">
+                <a href="https://github.com/Aaditya-Dhanraj" target="_blank">
+                  <img alt="" className="socialicon" src={github} />
+                </a>
+                <a href="https://www.google.com" target="_blank">
+                  <img alt="" className="socialicon" src={linkdin} />
+                </a>
+                <a
+                  href="https://www.instagram.com/its_aditude/"
+                  target="_blank"
+                >
+                  <img alt="" className="socialicon" src={instagram} />
+                </a>
+              </div>
             </Parallax>
           </div>
           <div className="right-side">
