5f48a318e07f78d5ac23dd3a24dd666a0dc29974
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2021-02-06 02:17:48 +0530

    graph added

--- src/sections/section2/Section2.js
+++ src/sections/section2/Section2.js
@@ -1,12 +1,52 @@
-import React from "react";
-import "./section2.css";
+import React, { useEffect, useState } from "react";
+import "../section3/section3.css";
+import { Line } from "react-chartjs-2";
 
 const Section2 = () => {
+  const data = {
+    labels: [
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB",
+    ],
+    datasets: [
+      {
+        label: "Skills",
+        lineTension: 0.3,
+        backgroundColor: "rgba(0,0,0,0.3)",
+        borderColor: "rgba(0,0,0,0.7)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(0,0,0,0.5)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(0,0,0,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 3,
+        pointBackgroundColor: "rgba(0,0,0,0.5)",
+        pointHitRadius: 30,
+        data: [80, 75, 85, 90, 70, 80, 70],
+      },
+    ],
+  };
+  const [wid, setWid] = useState(window.innerWidth);
+
+  useEffect(() => {
+    setWid(window.innerWidth);
+  }, [window.innerWidth]);
   return (
-    <div id="sec2" className="section2">
-      <div className="outter">
+    <div id="sec2" className="section3">
+      {console.log(wid)}
+      <div className="outter1">
         <span
-          className="inBox"
+          className="inBox1"
           style={{
             fontFamily: "Times New Roman, Times, serif",
             fontWeight: "bolder",
@@ -16,48 +56,32 @@ const Section2 = () => {
         >
           Technology Stack
         </span>
-        <div className="inner">
-          <div className="meter">
-            <span className="name">HTML5</span>
-            <div className="value ">
-              <div className="value1"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">CSS3</span>
-            <div className="value">
-              <div className="value2"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">JavaScript</span>
-            <div className="value ">
-              <div className="value3"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">React</span>
-            <div className="value ">
-              <div className="value4"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">Node</span>
-            <div className="value ">
-              <div className="value5"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">Express</span>
-            <div className="value ">
-              <div className="value6"></div>
-            </div>
-          </div>
-          <div className="meter">
-            <span className="name">MongoDB</span>
-            <div className="value ">
-              <div className="value7"></div>
-            </div>
+        <div className="inner1 inner3">
+          <div className="inner2 inner3">
+            <Line
+              id="canvas"
+              options={{
+                responsive: true,
+                scales: {
+                  xAxes: [
+                    {
+                      // gridLines: false,
+                    },
+                  ],
+                  yAxes: [
+                    {
+                      // gridLines: false,
+                      ticks: {
+                        min: 50,
+                        max: 100,
+                      },
+                    },
+                  ],
+                },
+              }}
+              height={wid < 450 ? 400 : null}
+              data={data}
+            />
           </div>
         </div>
       </div>
